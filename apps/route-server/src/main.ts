15da5bc56e5e9a3e6065d6ec1bbc40f7894081dc
aquashdw <aquashdw@gmail.com>
2021-07-16 14:20:38 +0900

    load proto files

--- apps/route-server/src/main.ts
+++ apps/route-server/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { join } from 'path';
 import { RouteServerModule } from './route-server.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(RouteServerModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    RouteServerModule,
+    {
+      transport: Transport.GRPC,
+      options: {
+        package: 'routeguide',
+        protoPath: join(__dirname, 'proto/route_guide.proto'),
+      },
+    },
+  );
+  await app.listen();
 }
 bootstrap();
